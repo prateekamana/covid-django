5f100d891a45ee5f76c415bec14b304d07ad9ae1
Prateek Amana <prateekamana@gmail.com>
2020-04-25 16:56:09 +0530

    middle-test-commit

--- main/views.py
+++ main/views.py
@@ -32,7 +32,7 @@ def single_slug(request, single_slug):
 def homepage(request):
 	return render(request=request,
 			template_name= "main/categories.html",
-			context={"categories": TutorialCategory.objects.all()}
+			context={}
 			)
 
 def register(request):
